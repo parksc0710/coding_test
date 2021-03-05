2904b8cfaf17c7fda46483728625f701ee446046
parks <parks@DESKTOP-6D57EQS>
2021-03-06 03:30:29 +0900

    풀이 성공

--- src/string/String_4446.java
+++ src/string/String_4446.java
@@ -14,6 +14,10 @@ import java.util.List;
  *	  
  */
 public class String_4446 {
+	
+	static List<Character> vList = Arrays.asList(new Character[]{'a', 'i', 'y', 'e', 'o', 'u'});
+	static List<Character> cList  = Arrays.asList(new Character[]{'b', 'k', 'x', 'z', 'n', 'h', 'd', 'c', 'w', 'g', 'p', 'v', 'j', 'q', 't', 's', 'r', 'l', 'm', 'f'});
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		List<String> inStringList = new ArrayList<String>();
@@ -23,11 +27,55 @@ public class String_4446 {
 			inStringList.add(temp);
 		}
 		
-		List<Character> vList = Arrays.asList(new Character[]{'a', 'i', 'y', 'e', 'o', 'u'});
-		List<Character> cList  = Arrays.asList(new Character[]{'b', 'k', 'x', 'z', 'n', 'h', 'd', 'c', 'w', 'g', 'p', 'v', 'j', 'q', 't', 's', 'r', 'l', 'm', 'f'});
-			
-		System.out.println(vList.toString());
-		System.out.println(cList.toString());
+		
+		for(int i = 0; i < inStringList.size(); i++) {
+			System.out.println(decoding(inStringList.get(i)));
+		}
 
+	}
+	
+	static String decoding(String before) {
+		
+		String rtnString = "";
+		
+		for(int i = 0; i < before.length(); i++) {
+			
+			boolean isUpperCase = false;
+			
+			Character tmpChar = before.charAt(i);
+			if(Character.isLetter(tmpChar)) {
+				if(vList.contains(Character.toLowerCase(tmpChar))) {
+					
+					if(Character.isUpperCase(tmpChar)) {
+						isUpperCase = true;
+					}
+					int tmpIndex = vList.indexOf(Character.toLowerCase(tmpChar));
+					int afterInex = tmpIndex + 3 > vList.size()-1 ? tmpIndex - 3 : tmpIndex + 3;
+					
+					if(isUpperCase) {
+						rtnString += String.valueOf(Character.toUpperCase(vList.get(afterInex)));
+					} else {
+						rtnString += String.valueOf(vList.get(afterInex));
+					}
+					
+				} else if(cList.contains(Character.toLowerCase(tmpChar))) {
+					if(Character.isUpperCase(tmpChar)) {
+						isUpperCase = true;
+					}
+					int tmpIndex = cList.indexOf(Character.toLowerCase(tmpChar));
+					int afterInex = tmpIndex + 10 > cList.size()-1 ? tmpIndex - 10 : tmpIndex + 10;
+					
+					if(isUpperCase) {
+						rtnString += String.valueOf(Character.toUpperCase(cList.get(afterInex)));
+					} else {
+						rtnString += String.valueOf(cList.get(afterInex));
+					}
+				}
+			} else {
+				rtnString += tmpChar;
+			}
+		}
+		
+		return rtnString;
 	}
 }
