2c02b55d6195e35a46d8cee08da6258daf38cc66
parks <parks@DESKTOP-6D57EQS>
2021-04-13 00:24:22 +0900

    프로그래머스 레벨 2 풀이 중

---
+++ src/lv2/Lv2_42586.java
@@ -0,0 +1,30 @@
+package lv2;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ *  - 일시 : 2021년 04월 12일
+ *  - 링크 : https://programmers.co.kr/learn/courses/30/lessons/42586
+ *	  
+ */
+public class Lv2_42586 {
+	public static void main(String[] args) throws IOException {
+		
+		int[] progresses = {93, 30, 55};
+		int[] speeds = {1, 30, 5};
+		Solution12901 tmp = new Solution12901();
+		System.out.println(tmp.solution(progresses, speeds));
+
+	}
+
+}
+
+class Solution12901 {
+	public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
